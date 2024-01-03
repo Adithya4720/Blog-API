968b0f4ec57b55ece1fa1a22fb3f9bbb7efee409
Adithya4720 <hegdeadithyak@gmail.com>
2024-01-03 17:40:41 +0530

    Files for Blog-api

---
+++ index.js
@@ -0,0 +1,9 @@
+const express =  require('express');
+const mongoose  = require('mongoose');
+
+mongoose.connect('mongodb://localhost/blog-api', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
